9f71d4e2e979794bf2e3f7f6061dbd6dd8f21d2d
brikkerdev <daniklyz@gmail.com>
2025-12-04 19:03:06 +0300

    Добавлены решения задач 9 и 10 практики про многопоточность

---
+++ Luzin/Practice_09_Threading/Task10.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luzin.Practice_09_Threading
+{
+    internal class Task10
+    {
+        SemaphoreSlim semaphoreSlim = new SemaphoreSlim(3);
+        Mutex mutex = new Mutex();
+        string value = "Данные";
+
+        public void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread reader = new Thread(Read);
+                reader.Name = "#" + i;
+                reader.Start();
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                Thread writer = new Thread(Write);
+                writer.Name = "#" + i;
+                writer.Start();
+            }
+        }
+
+        public void Read()
+        {
+            semaphoreSlim.Wait();
+
+            Console.WriteLine($"Читатель {Thread.CurrentThread.Name} прочитал {value}");
+
+            Thread.Sleep(1000);
+
+            semaphoreSlim.Release();
+        }
+
+        public void Write()
+        {
+            mutex.WaitOne();
+
+            value = Thread.CurrentThread.Name + " данные";
+            Console.WriteLine($"Писатель {Thread.CurrentThread.Name}: {value}");
+
+            Thread.Sleep(1000);
+
+            mutex.ReleaseMutex();
+        }
+    }
+}
---
+++ Luzin/Practice_09_Threading/Task9.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luzin.Practice_09_Threading
+{
+    internal class Task9
+    {
+        Semaphore sem = new Semaphore(4, 4);
+        Random random = new Random();
+
+        public void Park()
+        {
+            sem.WaitOne();
+
+            Console.WriteLine($"{Thread.CurrentThread.Name} Приехал на парковку");
+
+            Thread.Sleep(random.Next(3) * 1000);
+
+            Console.WriteLine($"{Thread.CurrentThread.Name} Уезжает с парковки");
+
+            sem.Release();
+        }
+
+        public void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread thread = new Thread(Park);
+                thread.Name = $"Автомобиль {i}";
+                thread.Start();
+            }
+        }
+    }
+}
